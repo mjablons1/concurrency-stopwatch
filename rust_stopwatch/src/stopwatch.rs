afc469daf7056db01d9a6fe6b8eaffc80208e22d
Pablon <p.jablonski.stw@gmail.com>
2024-05-11 17:13:58 +0200

    Stop watch rust version 1.0 with release targer build

--- rust_stopwatch/src/stopwatch.rs
+++ rust_stopwatch/src/stopwatch.rs
@@ -1,8 +1,23 @@
-use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+use std::io::stdin;
+use std::sync::atomic::{AtomicBool, AtomicI32, AtomicU64, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
+pub fn count_to_time(
+    count: i32,
+    counts_per_hour: i32,
+    counts_per_min: i32,
+    counts_per_sec: i32,
+) -> String {
+    let hours = (count / counts_per_hour).to_string();
+    let reminder = count % counts_per_hour;
+    let minutes = (reminder / counts_per_min).to_string();
+    let reminder2 = reminder % counts_per_min;
+    let seconds = (reminder2 / counts_per_sec).to_string();
+    let milisec = reminder2 % counts_per_sec;
+    hours + ":" + &minutes + ":" + &seconds + ":" + &milisec.to_string()
+}
 struct StopWatch {
     is_running: Arc<AtomicBool>,
     refresh_rate_msec: Arc<AtomicU64>,
@@ -10,19 +25,19 @@ struct StopWatch {
     counts_per_sec: i32,
     counts_per_min: i32,
     counts_per_hour: i32,
-    count: i32,
+    count: Arc<AtomicI32>,
 }
 
 impl StopWatch {
     fn new() -> StopWatch {
-        let mut sw = StopWatch {
+        let sw = StopWatch {
             is_running: Arc::new(AtomicBool::new(true)),
             refresh_rate_msec: Arc::new(AtomicU64::new(1000)),
             counter_rate_msec: Arc::new(AtomicU64::new(100)),
             counts_per_sec: 0,
             counts_per_min: 0,
             counts_per_hour: 0,
-            count: 0,
+            count: Arc::new(AtomicI32::new(0)),
         };
         StopWatch::set_counts(sw)
     }
@@ -34,53 +49,70 @@ impl StopWatch {
         return sw;
     }
 
-    pub fn count_to_time(&mut self) -> String {
-        let hours = (self.count / self.counts_per_hour).to_string();
-        let reminder = self.count % self.counts_per_hour;
-        let minutes = (reminder / self.counts_per_min).to_string();
-        let reminder2 = reminder % self.counts_per_min;
-        let seconds = (reminder2 / self.counts_per_sec).to_string();
-        let milisec = (reminder2 % self.counts_per_sec);
-        hours + ":" + &minutes + ":" + &seconds + ":" + &milisec.to_string()
-    }
-
-    pub fn counter(&mut self) {
-        let is_running_me = self.is_running.clone();
-        let counter_rate_msec = self.counter_rate_msec.clone();
-
-        thread::spawn(move || {
-            while is_running_me.load(Ordering::Relaxed) {
-                println!("count {}", counter_rate_msec.load(Ordering::Relaxed));
-                thread::sleep(Duration::from_millis(
-                    counter_rate_msec.load(Ordering::Relaxed),
-                ));
-            }
-        });
-    }
-
     pub fn refresh(&mut self) {
         let is_running_me = self.is_running.clone();
         let refresh_rate_msec = self.refresh_rate_msec.clone();
+        let count = self.count.clone();
+        let loc_counts_per_hour = self.counts_per_hour;
+        let loc_counts_per_min = self.counts_per_min;
+        let loc_counts_per_sec = self.counts_per_sec;
 
         thread::spawn(move || {
             while is_running_me.load(Ordering::Relaxed) {
-                println!("refresh");
+                println!(
+                    "{}",
+                    count_to_time(
+                        count.load(Ordering::Relaxed),
+                        loc_counts_per_hour,
+                        loc_counts_per_min,
+                        loc_counts_per_sec
+                    )
+                );
                 thread::sleep(Duration::from_millis(
                     refresh_rate_msec.load(Ordering::Relaxed),
                 ));
             }
         });
     }
+
+    pub fn counter(&mut self) {
+        let is_running_me = self.is_running.clone();
+        let count = self.count.clone();
+        let counter_rate_msec = self.counter_rate_msec.clone();
+
+        thread::spawn(move || {
+            while is_running_me.load(Ordering::Relaxed) {
+                thread::sleep(Duration::from_millis(
+                    counter_rate_msec.load(Ordering::Relaxed),
+                ));
+                let inc_count = count.load(Ordering::Relaxed);
+                count.store(inc_count + 1, Ordering::Relaxed);
+            }
+        });
+    }
+
+    pub fn read_input(&mut self) {
+        let mut resp = String::new();
+        loop {
+            match stdin().read_line(&mut resp) {
+                Ok(_) => {
+                    if resp.eq("q\n") {
+                        self.is_running.store(false, Ordering::Relaxed);
+                        break;
+                    } else if resp.eq("r\n") {
+                        self.count.store(0, Ordering::Relaxed);
+                    }
+                    resp = "".to_string();
+                }
+                Err(error) => println!("error: {error}"),
+            }
+        }
+    }
 }
 
 fn main() {
     let mut sw = StopWatch::new();
-    sw.counter();
     sw.refresh();
-    for i in 1..5 {
-        println!("main thread! {}", i);
-        thread::sleep(Duration::from_millis(1000));
-    }
-    sw.is_running.store(false, Ordering::Relaxed);
-    println!("end main");
+    sw.counter();
+    sw.read_input();
 }
